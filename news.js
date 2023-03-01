06e91f71e0f32fdf9ce2f28bd188ce752adde6bd
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-01 14:52:27 +0600

    Loaded Data

---
+++ news.js
@@ -0,0 +1,18 @@
+
+
+const loadData = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const datas = await res.json();
+    displayData(datas.data.news_category);
+};
+
+
+const displayData = datas => {
+    datas.forEach(data => {
+        console.log(data);
+    })
+}
+
+
+loadData()
