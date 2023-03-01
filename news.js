a69b1cd2c0de52414c8aba7a92debdea7594d88e
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-01 19:19:46 +0600

    Alert section

--- news.js
+++ news.js
@@ -11,20 +11,28 @@ const loadData = async () => {
 const displayData = categories => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         categoriesContainer.innerHTML += `
-        <a href="#" onClick="getId('${category.category_id}')"  class="nav-link">${category?.category_name}</a>
+        <a href="#" onClick="getId('${category.category_id}', '${category?.category_name}')"  class="nav-link">${category?.category_name}</a>
         `
 
     })
 }
 
 // get newsCategories id 
-const getId =async id => {
+const getId = async (id, name) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url);
     const categoryNews = await res.json();
-    console.log(categoryNews);
+    showAlertDetails(categoryNews.data, name);
 };
+
+
+// show category name and id inside alert box 
+const showAlertDetails = (data, name) => {
+    console.log(data, name);
+    document.getElementById('item-found-amount').innerText = data.length;
+    document.getElementById('item-found-name').innerText = name
+}
 
 
