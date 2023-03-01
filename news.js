655922c12b568f3124133e7e44761e9bbb01aaf1
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-01 18:00:42 +0600

    categories section added

--- news.js
+++ news.js
@@ -8,11 +8,23 @@ const loadData = async () => {
 };
 
 
-const displayData = datas => {
-    datas.forEach(data => {
-        console.log(data);
+const displayData = categories => {
+    const categoriesContainer = document.getElementById('categories-container');
+    categories.forEach(category => {
+        console.log(category);
+        categoriesContainer.innerHTML += `
+        <a href="#" onClick="getId('${category.category_id}')"  class="nav-link">${category?.category_name}</a>
+        `
+
     })
 }
 
+// get newsCategories id 
+const getId =async id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const res = await fetch(url);
+    const categoryNews = await res.json();
+    console.log(categoryNews);
+};
 
-// loadData()
+
